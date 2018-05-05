a6e7dbaccfd5068b972be82df88bedbde053bad7
Nelson Flamenco <Nelson Flamenco@Hp-PC>
2018-05-05 15:00:50 -0600

    ajustes visual de la interfaz

--- src/com/parcial1/Parcial1.java
+++ src/com/parcial1/Parcial1.java
@@ -12,9 +12,6 @@ import java.util.Scanner;
  * @author allan
 */
 public class Parcial1 {
-    /*
-     * @param args the command line arguments
-    */
     public static void main(String[] args) {
         // TODO code application logic here
         Engine system = new Engine();
@@ -22,19 +19,17 @@ public class Parcial1 {
         
         System.out.println("*****BIENVENIDO****");
         System.out.println("Antes de comenzar, desea iniciar con la configuracion por defecto?");
-        System.out.println("La configuracion por defecto agrega 6 pisos, 10 habitaciones por piso y ");
-        System.out.println("dos paquetes de servicios. Si no acepta la configuracion por defecto, ");
-        System.out.println("el sistema iniciara sin ninguna configuracion previa y todo se debera agregar manualmente");
+        System.out.println("La configuracion por defecto agrega 6 pisos con 10 habitaciones y dos paquetes de servicios.");
+        System.out.println("");
+        System.out.println("Si no acepta la configuracion por defecto,el sistema iniciara sin ninguna configuracion previa.");
+        System.out.println("Recomendacion: Solo usuarios avanzados...");
         System.out.println("");
         System.out.println("Desea inicial con la configuracion por defecto?");
-        System.out.println("Presione Y para aceptar, otra tecla para cancelar");
+        System.out.println("Presione Y para aceptar, otra tecla para omitir..");
         
-        if(in.nextLine().equals("Y")){
-            system.initSettings();
+        if(in.nextLine().equals("Y")){//system.initSettings contiene la configuracion inicial del proyecto de "HOTEL RAFINHA"
+            system.initSettings();    //Inicializando configuracion
         }
-        //Inicializando configuracion
-        //system.initSettings(6, 10);
-        //Iniciando sistema
-        system.initSystem();
+        system.initSystem();          //Iniciando sistema
     }
 }
